f5d7630daae0b2d999a2b5819eca7fadb057cab8
Romek <149534184+Romeofaiz@users.noreply.github.com>
2024-05-15 03:00:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,144 +1,85 @@
-const fs = require('fs-extra')
-if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
 
 
-//═══════[Required Variables]════════\\
-global.audio= "" ;  
-global.video= "" ;
-global.port =process.env.PORT
-global.appUrl=process.env.APP_URL || ""  // put your app url here,
-global.email ="romekxd@yahoo.com"
-global.location="Mumbai,india."
+global.owner = [
+  ['', 'ROMEK-XD', true], 
+  ['918433897160', 'ARYAN-XD', true], 
+  ['', 'ROMEK-XD', true],
+  ['918433897160', 'ARYAN-XD', true] ]
+
+ //Number of owners
+
+//global.pairingNumber = "" //put your bot number here
+
+global.mods = ['2341', '2348021779696', '2347045035241','233533763772'] 
+global.prems = ['2347045035241','233208179431','233533763772']
+global.allowed = ['2348021779696', '2347045035241','233533763772']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+global.beta = 'mLxstUwm'
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,        
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`, 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
+global.botname = '𝑅𝛩𝛭𝛯𝛫-𝛸𝐷'
+global.premium = 'true'
+global.packname = 'ROMEK-XD' 
+global.author = '919341378016' 
+global.menuvid = 'https://i.imgur.com/aZoNiJV.mp4'
+global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g\n' 
+global.dygp='▢✓ Follow My channel\nhttps://chat.whatsapp.com/Hbb23H4Ny4RIc3I83S9PEv\n'
+global.fgsc = 'https://github.com/Romeofaiz/ROMEK-XD-BOT' 
+global.fgyt = 'https://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g'
+global.fgpyp = 'https://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g'
+global.fglog = 'STAR.jpg' 
+global.thumb = fs.readFileSync('./STAR.jpg')
 
 
-global.mongodb= process.env.MONGODB_URI || ""
-global.allowJids= process.env.ALLOW_JID || "null" 
-global.blockJids= process.env.BLOCK_JID || "null"
+global.wait = '*🕣 _ROMEK-IS LOADING..._*\n*▰▰▰▱▱▱▱▱🕗*'
+global.rwait = '🔜'
+global.dmoji = '⚡'
+global.done = '☑️'
+global.error = '❎' 
+global.xmoji = '⤵️' 
 
+global.multiplier = 69 
+global.maxwarn = '3' // máxima advertencias
 
-global.DATABASE_URI = process.env.DATABASE_URL || ""
-
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Maharashtra";
-global.github=process.env.GITHUB|| "https://github.com/Romeofaiz/ROMEK-XD-BOT";
-global.gurl  = process.env.GURL || "https://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g";
-global.website= process.env.GURL|| "https://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/f1bb9bc8bd0d9a0a50eea.jpg" ; // SET LOGO FOR IMAGE 
-
-
-
-global.devs = "919341378016" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "919341378016";
-
-
-
-
-//========================= [ BOT SETTINGS ] =========================\\
-global.style = process.env.STYLE   || '5',  // put '1' to "5" here to check bot styles
-global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || "true";  // Make it "false" for disable WELCOME 
-
-global.warncount = process.env.WARN_COUNT || 3,
-global.disablepm = process.env.DISABLE_PM || "false", // disable bot in pm when public mode
-global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in groups when public mode
-
-global.MsgsInLog = process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
-global.userImages= process.env.USER_IMAGES|| "text", // set IMAGE/VIDEO links here
-global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
-
-
-//========================= [ AUTO READ MSGS & CMDS ] =========================\\
-global.readcmds = process.env.READ_COMMAND || "false"
-global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "919341378016,919xxxxxxxxx";
-
-
-//========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "false"
-global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,919xxxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "919341378016,919xxxxxxxxx";
-
-global.api_smd = "https://api-smd-1.vercel.app"
-global.scan = "https://suhail-md-vtsf.onrender.com/";
-
-
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
-
-
-module.exports = {
-
-  menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Romek-xd Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
-
-  HANDLERS: process.env.PREFIX  || ".",
-  BRANCH  : process.env.BRANCH  || "main",
-  VERSION : process.env.VERSION || "1.3.0",
-  caption : process.env.CAPTION || "©𝑅𝛩𝛭𝛯𝛫-𝛸𝐷-𝐵𝛩𝑇" , // ``
-  author : process.env.PACK_AUTHER|| "𝑅𝛩𝛭𝛯𝛫-𝛸𝐷-𝐵𝛩𝑇",
-  packname: process.env.PACK_NAME || "♥️",
-  botname : process.env.BOT_NAME  || "𝑅𝛩𝛭𝛯𝛫-𝛸𝐷-𝐵𝛩𝑇",
-  ownername:process.env.OWNER_NAME|| "𝑅𝛩𝛭𝛯𝛫-𝛸𝐷-𝐵𝛩𝑇",
-
-
-  errorChat : process.env.ERROR_CHAT || "",
-  KOYEB_API : process.env.KOYEB_API  || "false",
-
-  REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
-  antilink_values:process.env.ANTILINK_VALUES|| "all",
-  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
-
-  aitts_Voice_Id : process.env.AITTS_ID || "37",
-  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "private",
-  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "𝑅𝛩𝛭𝛯𝛫-𝐵𝛩𝑇",
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-global.rank = "updated"
-global.isMongodb = false; 
-let file = require.resolve(__filename)
-fs.watchFile(file, () => { fs.unwatchFile(file);console.log(`Update'${__filename}'`);delete require.cache[file];	require(file); })
- 
-
-// ========================= [ Disables in V.1.2.8 ] ===============================\\  
-  //style : process.env.STYLE || "2",  // put '1' & "2" here to check bot styles
-  //readmessage:process.env.READ_MESSAGE|| "false",
-  //warncount: process.env.WARN_COUNT || 3,
-  //userImages:process.env.USER_IMAGES|| "text",  // SET IMAGE AND VIDEO URL FOR BOT MENUS 
-  //disablepm: process.env.DISABLE_PM || "false",
-  //MsgsInLog: process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
-  //readcmds:process.env.READ_COMMANDS|| "false", 
-  //alwaysonline:process.env.WAPRESENCE|| "unavailable", // 'unavailable' | 'online' | 'composing' | 'recording' | 'paused'
-  //read_status: process.env.AUTO_READ_STATUS || "false",
-  //save_status: process.env.AUTO_SAVE_STATUS || "false",
- 
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
